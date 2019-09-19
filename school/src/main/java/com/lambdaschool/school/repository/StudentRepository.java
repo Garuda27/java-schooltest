050aff0007c31a856a068de8db3a7ca7f42d78fc
Preston <pres.10.nap@gmail.com>
2019-09-19 17:15:12 -0400

    mvp

--- school/src/main/java/com/lambdaschool/school/repository/StudentRepository.java
+++ school/src/main/java/com/lambdaschool/school/repository/StudentRepository.java
@@ -4,9 +4,8 @@ import com.lambdaschool.school.model.Student;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public interface StudentRepository extends CrudRepository<Student, Long>
 {
-    List<Student> findByStudnameContainingIgnoreCase(String name);
+    ArrayList<Student> findStudentsByStudnameEquals(String name);
 }
